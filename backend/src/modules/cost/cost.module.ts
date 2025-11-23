cb4f39135c9886a5c2929caa98cbf86b04ff0ea1
MOZTECHcompany <moztecheason@gmail.com>
2025-11-23 06:40:37 +0000

    修改2

--- backend/src/modules/cost/cost.module.ts
+++ backend/src/modules/cost/cost.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CostService } from './cost.service';
 import { CostRepository } from './cost.repository';
 import { PrismaModule } from '../../common/prisma/prisma.module';
+import { InventoryModule } from '../inventory/inventory.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, InventoryModule],
   providers: [CostService, CostRepository],
   exports: [CostService],
 })
